1eaa7888bb1b559d6f3d39dbd78cd12730c6fb70
lieutri191 <128084280+lieutri191@users.noreply.github.com>
2025-04-27 21:17:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,55 @@ function addToCart(productName) {
     alert(productName + " đã thêm vào giỏ hàng!");
   }
 
+  /* --- Slide khuyến mãi --- */
+  const slide = document.querySelector('.promo-slide');
+  const images = document.querySelectorAll('.promo-img');
+  const nextBtn = document.querySelector('.next-btn');
+  const prevBtn = document.querySelector('.prev-btn');
+  let index = 0;
+  let autoSlideInterval;
+
+  function showSlide(i) {
+    if (slide) {
+      slide.style.transform = `translateX(-${i * 100}%)`;
+    }
+  }
+
+  function nextSlide() {
+    index = (index + 1) % images.length;
+    showSlide(index);
+  }
+
+  function prevSlide() {
+    index = (index - 1 + images.length) % images.length;
+    showSlide(index);
+  }
+
+  function startAutoSlide() {
+    autoSlideInterval = setInterval(nextSlide, 3000);
+  }
+
+  function stopAutoSlide() {
+    clearInterval(autoSlideInterval);
+  }
+
+  function resetAutoSlide() {
+    stopAutoSlide();
+    startAutoSlide();
+  }
+
+  if (slide && images.length > 0 && nextBtn && prevBtn) {
+    nextBtn.addEventListener('click', () => {
+      nextSlide();
+      resetAutoSlide();
+    });
+    prevBtn.addEventListener('click', () => {
+      prevSlide();
+      resetAutoSlide();
+    });
+    startAutoSlide();
+  }
+
 /* giỏ hàng */
 document.addEventListener("DOMContentLoaded", function() {
     function updateTotal() {
